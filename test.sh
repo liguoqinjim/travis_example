2add25f1ec7a8af0595c4afb41630b2b2f76f3ea
liguoqinjim <liguoqinjim23@gmail.com>
2018-07-19 18:49:25 +0800

    codecov

--- test.sh
+++ test.sh
@@ -4,19 +4,19 @@ set -e
 echo "" > coverage.txt
 
 # 不在测试覆盖率里面去除lab002
-#for d in $(go list ./... | grep -v vendor); do
-#    go test -race -coverprofile=profile.out -covermode=atomic $d
-#    if [ -f profile.out ]; then
-#        cat profile.out >> coverage.txt
-#        rm profile.out
-#    fi
-#done
-
-# 去除lab002
-for d in $(go list ./... | grep -v vendor | grep -v lab002); do
+for d in $(go list ./... | grep -v vendor); do
     go test -race -coverprofile=profile.out -covermode=atomic $d
     if [ -f profile.out ]; then
         cat profile.out >> coverage.txt
         rm profile.out
     fi
 done
+
+# 去除lab002
+#for d in $(go list ./... | grep -v vendor | grep -v lab002); do
+#    go test -race -coverprofile=profile.out -covermode=atomic $d
+#    if [ -f profile.out ]; then
+#        cat profile.out >> coverage.txt
+#        rm profile.out
+#    fi
+#done
